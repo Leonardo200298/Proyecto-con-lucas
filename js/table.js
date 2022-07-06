488fbd1e7c89eee988d6f0cd21debbac86696b5e
Leonardo <leonardosanchez119@gmail.com>
2022-07-06 18:51:12 -0300

    first commit

--- js/table.js
+++ js/table.js
@@ -1,90 +1,116 @@
-"use strict";
-// Botones
-let botonAgregar = document.querySelector("#boton-agregar");
-botonAgregar.addEventListener('click', agregar);
-let botonBorrar = document.querySelector("#boton-borrar");
-botonBorrar.addEventListener('click', borrar);
-let botonx3 = document.querySelector("#boton-agregar-x3");
-botonx3.addEventListener('click', x3);
-let vaciarTabla = document.querySelector("#boton-vaciar-tabla");
-vaciarTabla.addEventListener('click', vaciar);
+//llamados
+document.querySelector("#boton-agregar").addEventListener('click', agregarUsuario);
 
-// Objeto json
+const URL = 'https://62b382f5a36f3a973d234233.mockapi.io/api/usuarios';
 
-let objTable = [{
-    nombre: "Cristian",
-    apellido: "Taboada",
-    edad: 21,
-    equipo: "Boca"
-},
-{
-    nombre: "Rafael",
-    apellido: "Cayuela",
-    edad: 21,
-    equipo: "Villa Dalmine"
-},
-{
-    nombre: "Bryan",
-    apellido: "Tolosa",
-    edad: 20,
-    equipo: "San Lorenzo"
-}]
-
-mostrar();
-function agregar(e) {
-    e.preventDefault();
-    let formData = document.querySelector("#form");
-    let forms = new FormData(formData);
-    let personaNueva = {
-        nombre: forms.get("nombre"),
-        apellido: forms.get("apellido"),
-        edad: forms.get("edad"),
-        equipo: forms.get("equipo")
+async function cargarTabla() {
+    try{
+        let promise = await fetch(URL);
+        let apiInfo = await promise.json();
+        document.querySelector("#table").innerHTML = "";
+        for (let index = 0; index < apiInfo.length; index++) {
+            document.querySelector("#table").innerHTML += `<tr>
+            <td>${apiInfo[index].name}</td>
+            <td>${apiInfo[index].lastName}</td>
+            <td>${apiInfo[index].age}</td>
+            <td>${apiInfo[index].team}</td>
+            <td><button class="borrar" id="${apiInfo[index].id}">borrar</button></td>
+            <td><button class="editar" id="${apiInfo[index].id}">editar</button></td>
+    
+            </tr>`
+        }
+        document.querySelectorAll(".borrar").forEach(boton=>{
+            boton.addEventListener('click',borrar);
+        });
+        document.querySelectorAll(".editar").forEach(botonE =>{
+            botonE.addEventListener('click', editar);
+        });
     }
-    objTable.push(personaNueva);
-    mostrar();
-}
-
-function x3(e) {
-    e.preventDefault();
-    let formData = document.querySelector("#form");
-    let forms = new FormData(formData);
-    let personaNueva = {
-        nombre: forms.get("nombre"),
-        apellido: forms.get("apellido"),
-        edad: forms.get("edad"),
-        equipo: forms.get("equipo")
+    catch(error){
+        console.log(error);
     }
-    for (let index = 0; index <= 2; index++) {
-        objTable.push(personaNueva);
-
-    }
-    mostrar();
 }
-
-function borrar(e) {
+async function agregarUsuario(e) {
     e.preventDefault();
-    objTable.pop();
-    mostrar();
-}
+    let formulario = document.querySelector("#form");
+    let formData = new FormData(formulario);
+    let nombre = formData.get("nombre");
+    let apellido = formData.get("apellido");
+    let edad = formData.get("edad");
+    let equipo = formData.get("equipo");
+    let obj = {
+        "name": nombre,
+        "lastName": apellido,
+        "age": edad,
+        "team": equipo
+    };
+    try {
+        let res = await fetch(URL, {
+            'method': 'POST',
+            'headers': { 'Content-Type': 'application/json' },
+            'body': JSON.stringify(obj)
 
-function vaciar(e) {
-    e.preventDefault();
-    objTable = [];
-    mostrar();
-}
-
-function mostrar() {
-    let htmlTable = document.querySelector("#table");
-
-    htmlTable.innerHTML = "";
-    for (const values of objTable) {
-        htmlTable.innerHTML += "<tr>" + "<td>" + values.nombre + "</td>"
-            + "<td>" + values.apellido + "</td>" +
-            "<td>" + values.edad + "</td>" +
-            "<td>" + values.equipo + "</td>" + "</tr>";
+        })
+        if (res.status == 201) {
+            console.log("creado!");
+        }
     }
-
+    catch (error) {
+        console.log(error);
+    }
+    cargarTabla();
 }
+async function borrar(e) {
+    e.preventDefault();  
+    try {
 
+        let res = await fetch(`${URL}/${this.id}/`, {
+            'method': 'DELETE',    
+        })
+        if (res.status === 200) {
+            console.log("Borrado");
+        }
+        else {
+            console.log("Fila no encontrada");
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+    cargarTabla();
+}
+async function editar(e){
+    e.preventDefault();
+    let formulario = document.querySelector("#form");
+    let formData = new FormData(formulario);
+    let nombre = formData.get("nombre");
+    let apellido = formData.get("apellido");
+    let edad = formData.get("edad");
+    let equipo = formData.get("equipo");
+    let obj = {
+        "name": nombre,
+        "lastName": apellido,
+        "age": edad,
+        "team": equipo
+    };
+    try {
+
+        let res = await fetch(`${URL}/${this.id}/`, {
+            'method': 'PUT',
+            'headers': { 'Content-Type': 'application/json' },
+            'body': JSON.stringify(obj)
+        });
+        if (res.status === 200) {
+            console.log("Borrado");
+        }
+        else {
+            console.log("Fila no encontrada");
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+    cargarTabla();
+}
+cargarTabla();
 
